a0f664af2a5400ffbe9323168b583cd4ecf24334
AnsHrnj <kanfyn@hotmail.fi>
2024-02-29 23:24:43 +0200

    Henkilöiden lisäys toimii

--- mongo.js
+++ mongo.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 5) {
-  console.log('give password, name and number as arguments')
-  process.exit(1)
-}
-
 const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
 const url =
   `mongodb+srv://AnsHrnj:${password}@fullstackreact.j1hafml.mongodb.net/PuhelinluetteloApp?retryWrites=true&w=majority&appName=FullstackReact`
@@ -15,24 +12,26 @@ mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
   name: String,
-  number: Number,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
-  name: 'testinimi',
-  number: 0,
+  name: name,
+  number: number,
 })
+
+/* if (process.argv.length === 3) {
+    Person.find({}).then(result => {
+      result.forEach(person => {
+        console.log(person)
+      })
+      mongoose.connection.close()
+    })    
+} */
 
 person.save().then(result => {
   console.log('person saved!')
-  mongoose.connection.close()
-})
-
-Person.find({}).then(result => {
-  result.forEach(person => {
-    console.log(person)
-  })
   mongoose.connection.close()
 })
